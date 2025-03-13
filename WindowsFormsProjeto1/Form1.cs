4f83e75dc0d8e5fa6adbbf78b5db34d1a1871486
jonatas-joao <jojokl@hotmail.com>
2025-03-12 22:11:44 -0300

    implementa form1

--- WindowsFormsProjeto1/Form1.cs
+++ WindowsFormsProjeto1/Form1.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace WindowsFormsProjeto1
 {
     public partial class Form1 : Form
@@ -7,19 +9,50 @@ namespace WindowsFormsProjeto1
             InitializeComponent();
         }
 
-        private void txtNomeDaMae_TextChanged(object sender, EventArgs e)
+        private void btnSalvar_Click(object sender, EventArgs e)
         {
+            if (!ValidaNome(txtNome.Text))
+                MessageBox.Show("Nome inválido");
+            //Remove os pontos e traços da máscara,
+            //para ler somente o valor que o usuário digitou
+            txtCPF.TextMaskFormat = MaskFormat.ExcludePromptAndLiterals;
+            
+            if (!ValidaCpf(txtCPF.Text))
+                MessageBox.Show("CPF Inválido");
+           
+        }
+
+        private bool ValidaNome(string nome)
+        {
+            if (nome == null || nome.Trim() == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool ValidaCpf(string cpf) 
+        {
+            if(cpf == null || cpf.Trim() == "" || !cpf.All(char.IsDigit) || cpf.Length!= 11)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
 
         }
 
-        private void txtNumeroMatricula_TextChanged(object sender, EventArgs e)
+        private void txtPaís_KeyPress(object sender, KeyPressEventArgs e)
         {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                MessageBox.Show("Apertou o Enter");
+            }
         }
     }
 }
